3ecbe1e579908397e2c27cc765b643605791a88c
ccq-ops <ccq_ouc@163.com>
2026-06-09 10:39:20 +0800

    refactor(docs): merge app entry into main

--- apps/docs/src/app.tsx
+++
@@ -1,7 +0,0 @@
-import { Router } from '@solidjs/router'
-import { Layout } from './components/layout'
-import { routes } from './routes'
-
-export function App() {
-  return <Router root={Layout}>{routes}</Router>
-}
--- apps/docs/src/main.tsx
+++ apps/docs/src/main.tsx
@@ -1,13 +1,16 @@
+import { Router } from '@solidjs/router'
 import { render } from 'solid-js/web'
 import { StyleProvider } from '@ant-design-solid/cssinjs'
-import { App } from './app'
+import { Layout } from './components/layout'
 import { DocsThemeProvider } from './components/theme-context'
-import './app.css'
+import { routes } from './routes'
+import './main.css'
+
 render(
   () => (
     <StyleProvider>
       <DocsThemeProvider>
-        <App />
+        <Router root={Layout}>{routes}</Router>
       </DocsThemeProvider>
     </StyleProvider>
   ),
