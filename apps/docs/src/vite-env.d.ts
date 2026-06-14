6ec53cdb20c89e3524bc8adcbb8c89e86ac24ee2
ccq-ops <ccq_ouc@163.com>
2026-06-14 18:08:48 +0800

    feat(docs): use mdx home page

--- apps/docs/src/vite-env.d.ts
+++ apps/docs/src/vite-env.d.ts
@@ -5,6 +5,8 @@ declare module '*.css'
 declare module '*.mdx' {
   import type { Component } from 'solid-js'
 
+  export const frontmatter: Record<string, unknown>
+
   const MDXComponent: Component<Record<string, unknown>>
   export default MDXComponent
 }
